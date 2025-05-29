5d86cf4872c290264115d2032a6ae5311c12ecf2
claramarcondees <carvalho.marcondes@escola.pr.gov.br>
2025-05-29 16:20:34 +0000

    aula 8

--- perguntas.js
+++ perguntas.js
@@ -1,7 +1,27 @@
+
+criaCartao(
+    'Ariana Grande',
+    'Quem é Ariana Grande',
+    'Ariana Grande é uma cantora, compositora e atriz americana conhecida por sua poderosa voz e sucessos no pop.'
+)
+
+
 criaCartao(
     'Ariana Grande',
     'Em que data Ariana Grande nasceu?',
     '26 de junho de 1993'
+)
+
+criaCartao(
+    'Ariana Grande',
+    'Onde ela nasceu?',
+    'Ariana Grande nasceu em Boca Raton, Flórida, Estados Unidos.'
+)
+
+criaCartao(
+    'Ariana Grande',
+    'Qual é o nome do irmão mais velho de Ariana, que também é artista?',
+    'Frankie Grande'
 )
 
 criaCartao(

da7589f1465da78f1455b0b5a7031835bfb02b3c
claramarcondees <carvalho.marcondes@escola.pr.gov.br>
2025-05-29 16:07:34 +0000

    aula 6

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Ariana Grande',
+    'Em que data Ariana Grande nasceu?',
+    '26 de junho de 1993'
+)
+
+criaCartao(
+    'Ariana Grande',
+    'Qual é o nome do primeiro álbum da Ariana Grande??',
+    'Yours Truly (2013)'
+)
+
+criaCartao(
+    'Ariana grande',
+    'Qual foi a série da Nickelodeon que deixou Ariana famosa?',
+    'Victorious, onde interpretava a Cat Valentine (2013)'
+)
